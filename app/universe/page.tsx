a90405fe20a30237720a3781c888c42efe3014e1
JimmyGithub007 <jimmygithub007@gmail.com>
2025-02-05 23:46:13 +0800

    20250205 - universe page add parallax effect & navbar

--- app/universe/page.tsx
+++ app/universe/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { FreeMode } from 'swiper/modules';
 import { motion } from 'framer-motion';
@@ -7,6 +8,7 @@ import { motion } from 'framer-motion';
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import Image from 'next/image';
+import { Navbar } from '@/components';
 
 const universes = [
     { id: 1, name: "TN7 WORLD LORE", url: "/assets/images/universe/world_lore.png" },
@@ -16,28 +18,55 @@ const universes = [
 ];
 
 const Universe = () => {
-    return (<div className="fixed flex justify-center items-center bg-cover bg-no-repeat h-screen top-0 w-full" style={{ backgroundImage: `url(./assets/images/universe/Background.png)` }}>
+    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+    useEffect(() => {
+        const handleMouseMove = (e: MouseEvent) => {
+            const { clientX, clientY } = e;
+            const x = clientX - window.innerWidth / 2;
+            const y = clientY - window.innerHeight / 2;
+            setMousePosition({ x, y });
+        };
+
+        window.addEventListener("mousemove", handleMouseMove);
+
+        return () => {
+            window.removeEventListener("mousemove", handleMouseMove);
+        };
+    }, []);
+
+    return (<div className="fixed flex justify-center items-center bg-cover bg-no-repeat h-screen top-0 w-full" style={{ backgroundImage: `url(./assets/images/universe/Background.png)` }}>
+        <Navbar setIsOpenMenuParent={setIsMenuOpen} isOpenMenuParent={isMenuOpen} />
         <Swiper
             loop={false}
             freeMode={true}
-            slidesPerView={3}
-            spaceBetween={30}
+            slidesPerView={4}
+            spaceBetween={20}
             centeredSlides={true}
-            modules={[ FreeMode ]}
+            modules={[FreeMode]}
         >
             {
                 universes?.map((value, key) => (
-                    <SwiperSlide key={key}>
-                        <div className="cursor-pointer relative h-[585.5px] w-[410.5px]">
-                            <Image className="absolute h-[585.5px] w-[410.5px]" alt={`universe`} width={1642} height={2342} src={value.url} priority />
+                    <SwiperSlide key={key}
+                    style={{
+                        transform: `translateX(${mousePosition.x * 0.02}px)`,
+                    }}
+                    >
+                        <motion.div
+                            className="cursor-pointer relative h-[585.5px] w-[410.5px]"
+                            initial={{ opacity: 0, x: "50px" }}
+                            whileInView={{ opacity: 1, x: 0 }}
+                            transition={{ duration: 0.5, delay: key * 0.1 }}
+                            viewport={{ once: true }}
+                        >
+                            <Image className="absolute duration-300 hover:saturate-200 h-[585.5px] w-[410.5px]" alt={`universe`} width={1642} height={2342} src={value.url} priority />
                             <div className="absolute bottom-16 font-bold w-full text-2xl text-center text-white">{value.name}</div>
-                        </div>
+                        </motion.div>
                     </SwiperSlide>
                 ))
             }
         </Swiper>
-
     </div>)
 }
 
