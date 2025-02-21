e76bc65ddfacbaf800e3f89ae75ede1f7879518b
JimmyGithub007 <jimmygithub007@gmail.com>
2025-02-21 10:55:39 +0800

    20250221 - world map update cities text & comic reader header add dropdown

--- app/comics/[id]/[chapter]/page.tsx
+++ app/comics/[id]/[chapter]/page.tsx
@@ -22,10 +22,15 @@ const content = [
 
 const ComicsChapter = () => {
     const [ isTop, setIsTop ] = useState<boolean>(true);
+    const [ isOpenDropDown, setIsOpenDropDown ] = useState<boolean>(true);
 
     const scrollToTop = () => {
         window.scrollTo({ top:0, behavior: "smooth" });
     }
+
+    useEffect(() => {
+        setIsOpenDropDown(false);
+    }, [isTop])
 
     useEffect(() => {
         const scrollEvent = () => {
@@ -46,11 +51,11 @@ const ComicsChapter = () => {
             <AnimatePresence>
             {   isTop && <motion.div  
                 initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -50 }} transition={{ duration: 0.5, delay: 0.2 }}
-                className="bg-black fixed grid grid-cols-3 h-16 items-center px-8 shadow-md shadow-slate-700/10 text-xl top-0 w-full" id="comicsHeader">
+                className="bg-black fixed grid grid-cols-3 h-16 items-center px-8 shadow-md shadow-slate-700/10 text-xl top-0 w-full relative z-[20]" id="comicsHeader">
                 <div className="flex items-center gap-2 text-white"><Link href={`/comics/1`}>SECRET OF THE VALLEY</Link> <IoIosArrowForward /> Episode 1</div>
                 <div className="flex gap-4 justify-center">
                     <button className="bg-white duration-300 hover:opacity-50 rounded-sm text-4xl"><IoIosArrowBack /></button>
-                    <button className="text-white">#1</button>
+                    <button onClick={() => { setIsOpenDropDown(!isOpenDropDown); }} className="text-white">#1</button>
                     <button className="bg-white duration-300 hover:opacity-50 rounded-sm text-4xl"><IoIosArrowForward /></button>
                 </div>
                 <div className="flex gap-4 items-center justify-end text-white">
@@ -58,6 +63,26 @@ const ComicsChapter = () => {
                     <FaXTwitter className="cursor-pointer duration-300 hover:opacity-50" />
                     <FaCopy className="cursor-pointer duration-300 hover:opacity-50" />
                 </div>
+                <AnimatePresence>
+                {
+                    isOpenDropDown && <motion.div
+                    initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }}
+                    className="absolute bg-black flex gap-8 h-36 items-center justify-center w-full top-16 text-white">
+                        <div className="cursor-pointer flex flex-col gap-2 group items-center opacity-30">
+                            <Image className="duration-300 group-hover:saturate-150 w-20" alt="prelude" height={315} width={315} src={`/assets/images/comics/prelude.png`} priority />
+                            <div>Prelude</div>
+                        </div>
+                        <div className="cursor-pointer flex flex-col gap-2 group items-center">
+                            <Image className="border-4 border-green-400 duration-300 group-hover:saturate-150 w-20" alt="prelude" height={315} width={315} src={`/assets/images/comics/prelude.png`} priority />
+                            <div className="text-green-400">Espisode 1</div>
+                        </div>
+                        <div className="cursor-pointer flex flex-col gap-2 group items-center opacity-30">
+                            <Image className="duration-300 group-hover:saturate-150 w-20" alt="prelude" height={315} width={315} src={`/assets/images/comics/prelude.png`} priority />
+                            <div>Espisode 2</div>
+                        </div>
+                    </motion.div>
+                }
+                </AnimatePresence>
             </motion.div> }                
             </AnimatePresence>
             <div className="max-w-[800px]">
