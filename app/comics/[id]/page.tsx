1c49f626b7b946980fdd2ebfae6d68151a0be143
JimmyGithub007 <jimmygithub007@gmail.com>
2025-03-13 07:45:58 +0800

    20250312 - ui enhancement - 4

--- app/comics/[id]/page.tsx
+++ app/comics/[id]/page.tsx
@@ -2,10 +2,12 @@
 
 import { Loader, Header, Footer } from "@/components";
 import { opinionPro } from "@/components/Font";
-import Image from "next/image";
-import Link from "next/link";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import { TbArrowBackUp } from "react-icons/tb";
+
+import Image from "next/image";
+import Link from "next/link";
 
 const content = [
     { id: "1", title: "PRELUDE", date: "07 FEB 2025" },
@@ -29,6 +31,7 @@ const ComicsId = () => {
     const calculateImgHeight = () => {
         // 使用图片的比例或任何逻辑动态计算高度
         let contentFrame = document.querySelector("img[alt='comicsFrameHorizontal']"); // 选择你的 ContentFrame 图片
+        console.log(window.innerWidth)
         if(window.innerWidth < 640) {
             setIsMobile(true);
             contentFrame = document.querySelector("img[alt='comicsFrameVertical']"); // 选择你的 ContentFrame 图片
@@ -59,23 +62,24 @@ const ComicsId = () => {
         <Header />
         <Loader />
         <Image id="background" className="absolute top-0 left-0 w-full h-full object-cover" alt="" width={5760} height={3260} src={`/assets/images/universe/webp/Background.webp`} priority />
-        <div className="relative max-w-[1200px] h-full flex items-center justify-center">
-            <Image className="invisible sm:visible absolute" alt="comicsFrameHorizontal" 
+        <div className="relative h-[calc(100%-80px)] flex items-center justify-center" style={{ width: isMobile ? "100%" : imgHeight * 2260/1379 }}>
+            <Image className="invisible sm:visible absolute xl:h-[-webkit-fill-available] w-auto" alt="comicsFrameHorizontal" 
                 height={1379} width={2260} src={`/assets/images/comics/${id}/webp/comicsFrameHorizontal.webp`}
                 placeholder="blur"
                 blurDataURL={`/assets/images/comics/${id}/webp/tiny/comicsFrameHorizontal.webp`}
             />
-            <Image className="sm:invisible absolute" alt="comicsFrameVertical" 
+            <Image className="sm:invisible absolute w-full h-auto" alt="comicsFrameVertical" 
                 height={2260} width={1379} 
                 src={`/assets/images/comics/${id}/webp/comicsFrameVertical.webp`} 
                 placeholder="blur"
                 blurDataURL={`/assets/images/comics/${id}/webp/tiny/comicsFrameVertical.webp`}
             />
-            <div className="grid grid-cols-1 grid-rows-2 sm:grid-rows-1 sm:grid-cols-2 items-center w-[80%]">
+            <div className="grid grid-cols-1 grid-rows-2 sm:grid-rows-1 sm:grid-cols-2 items-center">
                 <div className=""></div>
-                <div className={`filter-bar flex flex-col gap-8 md:justify-center overflow-y-auto overflow-x-hidden overflow-y-auto px-4 text-white z-10`}
-                    style={{ height: isMobile ? imgHeight * 45/100 : imgHeight * 80/100 }}
-                >
+                <div className={`filter-bar flex flex-col gap-8 md:justify-center px-[16%] text-white z-10 relative`}>
+                    <Link href={{ pathname: "/comics" }} className={`absolute cursor-pointer duration-300 hover:opacity-50 text-white text-4xl z-20 right-[16%] -top-[14%]`}>
+                        <TbArrowBackUp />
+                    </Link>
                     <div className={`flex flex-col gap-2`}>
                         <div className="text-lg sm:text-xl md:text-2xl xl:text-3xl">{comics.find(e => e.id === id)?.name || ""}</div>
                         <div className={`text-xs sm:text-sm md:text-md lg:text-lg italic ${opinionPro.className}`}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s</div>
